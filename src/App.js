5d6d48da8a9a57266187276e8fdb56cf95d4d6ae
Tricia Blosser <Tricia.blosser@gmail.com>
2020-09-23 14:06:09 -0400

    added switchNameHandler to change the state of names

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Tricia', age:"29" },
+      { name: 'Jeff', age:"30" },
+      { name: 'Max', age:"29" },
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("was clicked");
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Tricia's React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     /*  */return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "does this work now?"));
