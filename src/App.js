d7694801b2865514a19fb9eb2233ef09a984b5c7
Tricia Blosser <Tricia.blosser@gmail.com>
2020-10-01 14:34:38 -0400

    removing styled-components to add css modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 2px solid #ccc;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black
-  }
-`;
 
 class App extends Component {
   state = {
@@ -85,7 +70,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm Tricia's React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle People</StyledButton>
+        <button className="button" onClick={this.togglePersonsHandler}>Toggle People</button>
         {persons}
       </div>
     );
