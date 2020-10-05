65fe1a827c2424f9615e1b3189280061f5e017f2
Tricia Blosser <Tricia.blosser@gmail.com>
2020-10-05 16:17:11 -0400

    added error boundary/error message component in case of failure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -48,12 +49,14 @@ class App extends Component {
     persons = (
       <div >
         {this.state.persons.map((person, index) => {
-          return <Person
+          return <ErrorBoundary key={person.id}>
+            <Person
             click={(index) => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
             changed={(event) => this.nameChangedHandler(event, person.id)}>
           </Person>
+          </ErrorBoundary>
         })}
     </div>
     );
