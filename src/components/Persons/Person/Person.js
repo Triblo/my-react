0c00987b592c7b83e64b602b8d09abb139f67200
Tricia Blosser <Tricia.blosser@gmail.com>
2020-10-07 13:31:37 -0400

    added props passed to the HOC wrapped component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <Fragment>
+      <Aux>
         <p key="i1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old
         </p>
@@ -17,9 +18,9 @@ class Person extends Component {
           onChange={this.props.changed} 
           value={this.props.name}
         />
-      </Fragment>
+      </Aux>
     );
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
