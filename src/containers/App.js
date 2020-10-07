4d702a2d367edb0f6e7f63be23905223046ae24b
Tricia Blosser <Tricia.blosser@gmail.com>
2020-10-07 18:09:46 -0400

    adding authorization context to consumer

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import withClass from '../hoc/withClass';
 import Aux from '../hoc/Auxiliary';
+import AuthContext from '../context/auth-context';
+import authContext from '../context/auth-context';
 
 class App extends Component {
   constructor(props) {
@@ -88,7 +90,7 @@ class App extends Component {
         persons={this.state.persons}
         clicked={this.deletePersonHandler}
         changed={this.nameChangedHandler}
-        isAthenticated={this.state.authenticated}
+        isAuthenticated={this.state.authenticated}
       />;
   }
 
@@ -100,16 +102,22 @@ class App extends Component {
     }}
     >RemoveCockpit
     </button>
-    {this.state.showCockpit ? (
-      <Cockpit
-        title={this.props.appTitle}
-        showPersons={this.state.showPersons}
-        personsLength={this.state.persons.length}
-        clicked={this.togglePersonsHandler}
-        login={this.loginHandler}
-        />
-      ) : null }
-      {persons}
+    <AuthContext.Provider value={{
+      authenticated: this.state.authenticated,
+      login: this.loginHandler
+      }}
+    >
+      {this.state.showCockpit ? (
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          personsLength={this.state.persons.length}
+          clicked={this.togglePersonsHandler}
+          login={this.loginHandler}
+          />
+        ) : null }
+        {persons}
+    </AuthContext.Provider>
     </Aux>
     );
   }
